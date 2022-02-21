c1d8ce9a4109c5736f51fed4d46a381e992930e9
Arne Pedersen <arne.lamo@gmail.com>
2022-02-21 18:16:24 +0100

    Add content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const getImages = async (query) => {
+  // The base URL for our API
+  const url = "https://serverless-api.arnepedersen.workers.dev/";
+
+  const resp = await fetch(url, {
+    // Send a POST request
+    method: "POST",
+    // With a JSON-stringified body containing the query from our input
+    body: JSON.stringify({ query }),
+    // Set the `Content-type` header so our API knows that the request
+    // is sending JSON
+    headers: { "Content-type": "application/json" },
+  });
+  return resp.json();
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [images, setImages] = useState([]);
+
+  const search = async () => {
+    const results = await getImages(query);
+    setImages(results);
+  };
+
+  const updateQuery = (evt) => setQuery(evt.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="form">
+        <input
+          id="query"
+          type="text"
+          onChange={updateQuery}
+          placeholder="Search query"
+        />
+        <button onClick={search}>Search</button>
+      </div>
+
+      {/* Map through the array of images and render a set of images */}
+      {images.map((image) => (
+        <a key={image.id} href={image.link} target="_blank" rel="noreferrer">
+          <img src={image.image} alt="gitar" />
         </a>
-      </header>
+      ))}
     </div>
   );
 }
